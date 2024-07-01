5a5f263f0bda18188235e3bba9a972fccb6e1f4d
luizeduardosb <luizeduardo00736@gmail.com>
2024-07-01 15:33:55 -0300

    Content Negotiation

---
+++ firstStepsInJavaWithSpringBoot/rest-with-spring-boot/src/main/java/com/bezerra/util/MediaType.java
@@ -0,0 +1,8 @@
+package com.bezerra.util;
+
+public class MediaType {
+	public static final String APPLICATION_JSON = "application/JSON";
+	public static final String APPLICATION_XML = "application/xml";
+	public static final String APPLICATION_YML = "application/x-yaml";
+
+}
